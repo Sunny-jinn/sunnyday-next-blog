e6da2b311587f08a9174cd01ed528d3c85ace607
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-17 01:12:38 +0900

    refactor: 부모 Wrapper 삭제

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,33 +3,24 @@ import ReactTyped from 'react-typed';
 
 const Banner = () => {
   return (
-    <S.Wrapper>
-      {/* <Image
-        src="/assets/profile.JPG"
-        alt="프로필 사진"
-        width={300}
-        height={300}
-        style={{ borderRadius: 150 }}
-      /> */}
-      <S.Intro>
-        <div>HI 👋</div>
-        <div>
-          I am{' '}
-          <strong
-            style={{ backgroundColor: '#000', color: '#fff', paddingLeft: 10 }}
-          >
-            <ReactTyped
-              strings={['Frontend', 'Three.js', 'React', 'Typescript']}
-              typeSpeed={60}
-              backSpeed={40}
-              loop
-            />
-          </strong>
-          <br />
-          junior developer
-        </div>
-      </S.Intro>
-    </S.Wrapper>
+    <S.Intro>
+      <div>HI 👋</div>
+      <div>
+        I am{' '}
+        <strong
+          style={{ backgroundColor: '#000', color: '#fff', paddingLeft: 10 }}
+        >
+          <ReactTyped
+            strings={['Frontend', 'Three.js', 'React', 'Typescript']}
+            typeSpeed={60}
+            backSpeed={40}
+            loop
+          />
+        </strong>
+        <br />
+        junior developer
+      </div>
+    </S.Intro>
   );
 };
 
--- src/components/Banner/styled.ts
+++ src/components/Banner/styled.ts
@@ -1,12 +1,5 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
-  width: 100%;
-  height: 800px;
-  display: flex;
-  align-items: center;
-`;
-
 export const Intro = styled.div`
   font-size: 60px;
 `;
