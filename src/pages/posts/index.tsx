6b006036344c6ddaf78d237f1e6b4729aa0def79
Sunny-jinn <rlawlsdn316@gmail.com>
2024-01-01 23:57:51 +0900

    feat: posts 목록 style

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,17 +3,35 @@ import { PostData } from '@/types/types';
 import { NextPage } from 'next';
 
 import * as S from './styled';
+import Image from 'next/image';
+import Link from 'next/link';
+import { formatDate } from '@/api/date';
 
 const Posts: NextPage<{ posts: PostData[] }> = ({ posts }) => {
-  console.log(posts);
   return (
     <S.Wrapper>
       {posts.map((post, idx) => (
-        <S.Container key={idx}>
-          <div>{post.title}</div>
-          <div>{post.date}</div>
-          <div>{post.excerpt}</div>
-        </S.Container>
+        <Link href={`/posts/${post.slug}`} key={idx}>
+          <S.Container>
+            <Image
+              src={post.ogImage.url}
+              alt="이미지 불러오기 오류"
+              width={120}
+              height={120}
+              style={{
+                marginRight: 40,
+              }}
+            />
+            <div>
+              <S.PostTitle>
+                {post.title}
+                <S.PostDate>{formatDate(post.date)}</S.PostDate>
+              </S.PostTitle>
+
+              <S.PostExcerpt>{post.excerpt}</S.PostExcerpt>
+            </div>
+          </S.Container>
+        </Link>
       ))}
     </S.Wrapper>
   );
@@ -22,6 +40,13 @@ const Posts: NextPage<{ posts: PostData[] }> = ({ posts }) => {
 export default Posts;
 
 export const getStaticProps = () => {
-  const posts = getAllPosts(['slug', 'title', 'date', 'excerpt']);
+  const posts = getAllPosts([
+    'slug',
+    'title',
+    'date',
+    'excerpt',
+    'ogImage',
+    'category',
+  ]);
   return { props: { posts } };
 };
--- src/pages/posts/styled.ts
+++ src/pages/posts/styled.ts
@@ -6,8 +6,54 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding-top: 200px;
 `;
 
 export const Container = styled.div`
+  position: relative;
+  width: 900px;
+  height: 170px;
+  display: flex;
+  padding: 20px 0;
+
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    height: 0.5px;
+    background-color: rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+  }
+
+  &::before {
+    top: 0;
+  }
+
+  &::after {
+    bottom: 0;
+  }
+`;
+
+export const PostTitle = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  display: flex;
+  align-items: baseline;
+`;
+
+export const PostDate = styled.div`
+  font-size: 12px;
+  font-weight: 300;
+  color: #777;
+  margin-left: auto;
+`;
+
+export const PostExcerpt = styled.div`
   width: 100%;
 `;
