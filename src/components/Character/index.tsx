abf81580e3d676dd2f990804fd3b46597ca45f8c
Sunny-jinn <rlawlsdn316@gmail.com>
2024-01-08 22:08:27 +0900

    feat: 캐릭터, 돋보기 반응형 위치 및 크기 수정

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -2,7 +2,7 @@ import { RootState, useThree } from '@react-three/fiber';
 import { MyCharacter } from './MyCharacter';
 
 import { motion } from 'framer-motion-3d';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 type CharacterProps = {
   section: number;
@@ -11,6 +11,37 @@ type CharacterProps = {
 const Character = ({ section }: CharacterProps) => {
   const [animation, setAnimation] = useState<string>('Typing');
   const [isFirstLoading, setIsFirstLoading] = useState<boolean>(true); // 블로그 처음 방문 시 떨어지는 애니메이션 재생 안 되게
+
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+  });
+  const [position, setPosition] = useState({
+    x: 1.9,
+    y: 2,
+    z: 0,
+  });
+
+  const handleResize = useCallback(() => {
+    setWindowSize({
+      width: window.innerWidth,
+    });
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    if (windowSize.width <= 900) {
+      setPosition({ x: 1.5, y: 0.5, z: 0 });
+    } else {
+      setPosition({ x: 1.9, y: 2, z: 0 });
+    }
+  }, [windowSize]);
 
   const { viewport }: RootState = useThree();
 
@@ -37,9 +68,9 @@ const Character = ({ section }: CharacterProps) => {
       variants={{
         0: {},
         1: {
-          y: -viewport.height - 2,
-          x: 1.9,
-          z: 0,
+          y: -viewport.height - position.y,
+          x: position.x,
+          z: position.z,
           rotateX: 0.5,
           rotateY: -2.2,
           rotateZ: 0.5,
--- src/components/MagGlasses/index.tsx
+++ src/components/MagGlasses/index.tsx
@@ -1,15 +1,52 @@
+import { useCallback, useEffect, useState } from 'react';
 import { MagGlass } from './MagGlass';
 import { motion } from 'framer-motion-3d';
+import * as THREE from 'three';
 
 const MagGlasses = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+  });
+  const [position, setPosition] = useState(new THREE.Vector3(0.3, -2.7, 1));
+  const [scale, setScale] = useState(0.6);
+
+  const handleResize = useCallback(() => {
+    setWindowSize({
+      width: window.innerWidth,
+    });
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    if (windowSize.width <= 900) {
+      setScale(0.4);
+      setPosition(new THREE.Vector3(-0.3, -3.1, 1));
+    } else {
+      setScale(0.6);
+      setPosition(new THREE.Vector3(0.3, -2.7, 1));
+    }
+  }, [windowSize]);
+
   return (
     <motion.group
-      scale={0.6}
-      position={[0.3, -2.7, 1]}
+      scale={scale}
+      position={position}
+      // animate={{
+      //   x: [0.3, 0.5, 0.4, 0.3],
+      //   y: [-2.7, -3.0, -3.7, -2.7],
+      //   z: [1, 1, 1.2, 1],
+      // }}
       animate={{
-        x: [0.3, 0.5, 0.4, 0.3],
-        y: [-2.7, -3.0, -3.7, -2.7],
-        z: [1, 1, 1.2, 1],
+        x: [position.x, position.x + 0.2, position.x + 0.1, position.x],
+        y: [position.y, position.y - 0.5, position.y - 1, position.y],
+        z: [position.z, position.z, position.z + 0.2, position.z],
       }}
       transition={{
         repeat: Infinity,
