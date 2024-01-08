544a8b473727041c8803427abae738e94a709ded
Sunny-jinn <rlawlsdn316@gmail.com>
2024-01-08 21:23:04 +0900

    feat: office, character component 반응형 위치 및 크기 조정

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,17 +3,47 @@ import { Canvas } from '@react-three/fiber';
 import Character from '../Character';
 import Layout from '../Layout';
 import Interface from '../Interface';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import ScrollManager from '../ScrollManager';
 import { Office } from '../Character/Office';
 import { Cursor } from '../Cursor';
 import MagGlasses from '../MagGlasses';
 import { myFont } from '@/pages/_app';
-
+import * as THREE from 'three';
 import * as S from './styled';
 
 const Map = () => {
   const [section, setSection] = useState<number>(0);
+
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+  });
+  const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0));
+  const [scale, setScale] = useState(1);
+
+  const handleResize = useCallback(() => {
+    setWindowSize({
+      width: window.innerWidth,
+    });
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    if (windowSize.width <= 900) {
+      setScale(0.7);
+      setPosition(new THREE.Vector3(-1, -1.5, 0));
+    } else {
+      setScale(1);
+      setPosition(new THREE.Vector3(0, 0, 0));
+    }
+  }, [windowSize]);
 
   return (
     <>
@@ -21,20 +51,16 @@ const Map = () => {
         <Layout></Layout>
       </main>
       <S.Wrapper>
-        <Canvas
-          shadows
-          style={{
-            transform: '',
-          }}
-          className="canvas"
-        >
+        <Canvas shadows>
           <ambientLight intensity={2} />
           <directionalLight position={[-5, 5, 5]} intensity={4} castShadow />
           <ScrollControls pages={4} damping={0.1}>
             <ScrollManager section={section} onSectionChange={setSection} />
             <Scroll>
-              <Office />
-              <Character section={section} />
+              <group scale={scale} position={position}>
+                <Office />
+                <Character section={section} />
+              </group>
               <MagGlasses />
               <mesh position={[0, -7.7, 0]}>
                 <planeGeometry args={[15, 7.73]} />
--- src/components/Map/styled.ts
+++ src/components/Map/styled.ts
@@ -4,10 +4,4 @@ import styled from '@emotion/styled';
 export const Wrapper = styled.div`
   width: 100vw;
   height: 100%;
-
-  @media ${TABLET_MEDIA_QUERY} {
-    .canvas {
-      height: 200px !important;
-    }
-  }
 `;
